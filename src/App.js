30f63faf81db486e5f8f40882638b9f28aa1b319
Torjus Eidet <torjue@gmail.com>
2019-04-04 00:46:58 +0200

    Human init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import playlists from './playlists.json';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        { playlists.map(playlist => 
+          <div className="entry">
+            <div className="info">
+              <div className="author">{playlist.year}: {playlist.author}</div>
+              <div className="title">{playlist.title}</div>
+            </div>
+            <div className="playlist">
+              <iframe src={`https://open.spotify.com/embed?uri=spotify:playlist:${playlist.id}`} width="400px" height="350px" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+          </div>
+        ) }
+      </>
+    );
+  }
+}
+
+export default App;
