2c45d6f93b64f592a39639932b887ac336d223a4
Nilsen, Sondre <sondre.nilsen@sbanken.no>
2019-07-03 08:42:43 +0200

    Add prettier and format everything

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import playlists from './playlists.json';
+import React, { Component } from "react";
+import "./App.css";
+import playlists from "./playlists.json";
 
 class App extends Component {
   render() {
     return (
       <div className="entries">
-        { playlists.sort((pa, pb) => pb.id - pa.id).map(playlist => 
-          <div className="entry">
-            <div className="info">
-              <div className="author">{playlist.year}: {playlist.author}</div>
-              <div className="title">{playlist.title}</div>
+        {playlists
+          .sort((pa, pb) => pb.id - pa.id)
+          .map(playlist => (
+            <div className="entry">
+              <div className="info">
+                <div className="author">
+                  {playlist.year}: {playlist.author}
+                </div>
+                <div className="title">{playlist.title}</div>
+              </div>
+              <div className="playlist">
+                <iframe
+                  title={playlist.title}
+                  src={`https://open.spotify.com/embed?uri=spotify:playlist:${playlist.spotifyid}`}
+                  width="400px"
+                  height="350px"
+                  frameborder="0"
+                  allowtransparency="true"
+                  allow="encrypted-media"
+                />
+              </div>
             </div>
-            <div className="playlist">
-              <iframe title={playlist.title} src={`https://open.spotify.com/embed?uri=spotify:playlist:${playlist.spotifyid}`} width="400px" height="350px" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-            </div>
-          </div>
-        ) }
+          ))}
       </div>
     );
   }
